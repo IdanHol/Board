46c6f0266c8e5f069698665e6b1433407d9329d4
IdanHol <33767899+IdanHol@users.noreply.github.com>
2018-06-04 02:13:47 +0300

    bug fix

--- Board.cpp
+++ Board.cpp
@@ -87,14 +87,14 @@ bool existFile (const string& nameFile) {
 string  Board:: draw(unsigned int pixels){
 
     int i = 0;
-    string nameFile="";
+    string nameFile="1";
     nameFile+=to_string(i);
     nameFile+=".ppm";
 
     while(existFile(nameFile)){
         i++;
         nameFile.clear();
-        nameFile="";
+        nameFile="1";
         nameFile+=to_string(i);
         nameFile+=".ppm";
     }
